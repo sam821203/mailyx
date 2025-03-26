700f6fca1aa145c1a70381b997e6c082e57f0718
Sam Huang (黃宇浩) <sam821203@yahoo.com.tw>
2025-03-26 11:33:35 +0800

    feat: 新增inbox相關組件

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,5 +5,10 @@ export const routes: Routes = [
     path: "auth",
     loadChildren: () => import("./auth/auth.module").then((m) => m.AuthModule),
   },
-  { path: "", redirectTo: "auth", pathMatch: "full" },
+  {
+    path: "inbox",
+    loadChildren: () => import("./inbox/inbox.module").then((m) => m.InboxModule),
+  },
+  { path: "", redirectTo: "inbox", pathMatch: "full" },
+  { path: "**", redirectTo: "inbox/home" },
 ]
