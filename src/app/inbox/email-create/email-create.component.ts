b22a93db96811398a67ff8221b814c7fd4da11ec
Sam Huang (黃宇浩) <sam821203@yahoo.com.tw>
2025-04-01 14:57:21 +0800

    feat: 新增信件相關元件創建

---
+++ src/app/inbox/email-create/email-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-create',
+  imports: [],
+  templateUrl: './email-create.component.html',
+  styleUrl: './email-create.component.scss'
+})
+export class EmailCreateComponent {
+
+}
