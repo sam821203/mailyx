420cd6addc0933bb8d7ada31295d988941d09a3a
Sam Huang (黃宇浩) <sam821203@yahoo.com.tw>
2025-04-02 11:33:42 +0800

    feat: 新增email首頁

--- src/app/inbox/email-index/email-index.component.ts
+++ src/app/inbox/email-index/email-index.component.ts
@@ -1,14 +1,20 @@
 import { Component } from "@angular/core"
 import { EmailService } from "../email.service"
+import { EmailSummary } from "../email.service"
+import { CommonModule } from "@angular/common"
 
 @Component({
   selector: "app-email-index",
-  imports: [],
+  imports: [CommonModule],
   templateUrl: "./email-index.component.html",
   styleUrl: "./email-index.component.scss",
 })
 export class EmailIndexComponent {
+  emails: EmailSummary[] = []
+
   constructor(private emailService: EmailService) {
-    this.emailService.getEmails().subscribe(() => {})
+    this.emailService.getEmails().subscribe((emails) => {
+      this.emails = emails
+    })
   }
 }
